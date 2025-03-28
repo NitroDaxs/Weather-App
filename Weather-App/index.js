195643602e44a28d0d735d3c0512bc0e147bcb54
NitroDaxs <miroslav1423@gmail.com>
2025-03-28 20:27:23 +0200

    Finished with most of the design

--- Weather-App/index.js
+++ Weather-App/index.js
@@ -3,12 +3,26 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+const APIid = "154ef6b2bbe1b6115aea8e0f50c32f4f";
 
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   try {
     res.render("index.ejs");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/weather", async (req, res) => {
+  try {
+    const city = req.query.city;
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIid}`
+    );
+    const data = response.data;
+    res.render("index.ejs", { data });
   } catch (err) {
     console.log(err);
   }
